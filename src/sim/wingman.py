b5f9e3b955f466fd5119de90d7f6579a84d9b444
István Váradi <ivaradi@varadiistvan.hu>
2019-01-07 11:40:53 +0100

    Fixed event code specification in the WingMan simulator.

--- src/sim/wingman.py
+++ src/sim/wingman.py
@@ -6,9 +6,9 @@ from evdev import ecodes
 
 if __name__ == "__main__":
     events = {
-        ecodes.EV_ABS: [(ecodes.ABS_X, (0, 255, 0, 0)),
-                        (ecodes.ABS_Y, (0, 255, 0, 0)),
-                        (ecodes.ABS_Z, (0, 255, 0, 0))],
+        ecodes.EV_ABS: [(ecodes.ABS_X, (0, 128, 255, 0)),
+                        (ecodes.ABS_Y, (0, 128, 255, 0)),
+                        (ecodes.ABS_Z, (0, 128, 255, 0))],
         ecodes.EV_KEY: [ecodes.BTN_PINKIE, ecodes.BTN_BASE, ecodes.BTN_TRIGGER,
                         ecodes.BTN_TOP, ecodes.BTN_TOP2, ecodes.BTN_THUMB,
                         ecodes.BTN_THUMB2]

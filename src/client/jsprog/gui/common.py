46abf118c995fc86e6a3e9c982550efa2643c3fc
István Váradi <ivaradi@varadiistvan.hu>
2019-09-03 20:01:34 +0200

    Gdk is also imported in common.py

--- src/client/jsprog/gui/common.py
+++ src/client/jsprog/gui/common.py
@@ -20,6 +20,8 @@ import gettext
 appIndicator = False
 
 import gi
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gdk
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import Gio

fbe8b0d2a473412acaf4f2c76a59159552a97d31
István Váradi <ivaradi@varadiistvan.hu>
2021-02-28 10:07:10 +0100

    Build script for Debian binary packages

---
+++ scripts/build/debian-binary.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -u -x
+
+distribution="${1}"
+
+cd "${DRONE_WORKSPACE}"
+
+git config --global user.email "drone@nowhere.org"
+git config --global user.name "Drone User"
+
+git submodule update --init
+
+git fetch origin debian/dist/${distribution}/${DRONE_TARGET_BRANCH}
+git checkout origin/debian/dist/${distribution}/${DRONE_TARGET_BRANCH}
+
+git merge ${DRONE_COMMIT} < /dev/null
+
+export DEBIAN_FRONTEND=noninteractive
+echo "y" | mk-build-deps --install debian/control
+
+debuild -us -uc -b
